25a27fcff80c35e323eec94ce17491ea16ccc976
ibukin <ibukin@voice-link.ru>
2022-04-03 14:47:00 +0300

    Work Complete

--- api.py
+++ api.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import abc
-import inspect
 import json
 import datetime
 import logging
@@ -12,26 +10,12 @@ from http import server, HTTPStatus
 from optparse import OptionParser
 from weakref import WeakKeyDictionary
 
-from scoring import get_score
+from scoring import get_score, get_interests
 
 SALT = "Otus"
 ADMIN_LOGIN = "admin"
 ADMIN_SALT = "42"
 
-# Todo up to HTTPStatus
-OK = 200
-BAD_REQUEST = 400
-FORBIDDEN = 403
-NOT_FOUND = 404
-INVALID_REQUEST = 422
-INTERNAL_ERROR = 500
-ERRORS = {
-    BAD_REQUEST: "Bad Request",
-    FORBIDDEN: "Forbidden",
-    NOT_FOUND: "Not Found",
-    INVALID_REQUEST: "Invalid Request",
-    INTERNAL_ERROR: "Internal Server Error",
-}
 UNKNOWN = 0
 MALE = 1
 FEMALE = 2
@@ -47,12 +31,6 @@ class Field:
         self.required = required
         self.nullable = nullable
 
-    def check_constrains(self, value):
-        if self.required and not value:
-            raise ValueError(f"Field {self.__class__.__name__} is required")
-        if self.nullable and not value:
-            raise ValueError(f"Field {self.__class__.__name__} cannot be empty")
-
 
 class CharField(Field):
     def __init__(self, **kwargs):
@@ -63,12 +41,26 @@ class CharField(Field):
         return self.data.get(instance)
 
     def __set__(self, instance, value):
-        if type(value) not in (str, int):
-            raise ValueError(f"Invalid value for {self.__class__.__name__}: {type(value).__name__}")
+        self.check_constrains(value)
         if self.nullable and not value:
             self.data[instance] = ''
         else:
+            self.valid_field(value)
+
+        if type(value) not in (str, int, dict):
+            raise ValueError(f"Invalid value for {self.__class__.__name__}: {type(value).__name__}")
+        else:
             self.data[instance] = value
+
+    def check_constrains(self, value):
+        if self.required and not value:
+            raise ValueError(f"Field {self.__class__.__name__} is required")
+        if not self.nullable and not value:
+            raise ValueError(f"Field {self.__class__.__name__} cannot be empty")
+
+    @staticmethod
+    def valid_field(value):
+        pass
 
 
 class EmailField(CharField):
@@ -79,7 +71,7 @@ class EmailField(CharField):
             return True
 
 
-class ArgumentsField(Field):
+class ArgumentsField(CharField):
     def valid_field(self, value):
         try:
             json.dumps(value)
@@ -90,7 +82,7 @@ class ArgumentsField(Field):
             raise ValueError(err_msg)
 
 
-class PhoneField(Field):
+class PhoneField(CharField):
     def valid_field(self, value):
         err_msg = []
         if not type(value) in (str, int):
@@ -106,7 +98,7 @@ class PhoneField(Field):
             return True
 
 
-class DateField(Field):
+class DateField(CharField):
     def valid_field(self, value):
         try:
             date = datetime.datetime.strptime(value, "%d.%m.%Y")
@@ -115,7 +107,7 @@ class DateField(Field):
             raise ValueError(f"{self.__class__.__name__} {value} does not match format '%d.%m.%Y'")
 
 
-class BirthDayField(Field):
+class BirthDayField(CharField):
     def valid_field(self, value):
         try:
             date = datetime.datetime.strptime(value, "%d.%m.%Y")
@@ -129,7 +121,7 @@ class BirthDayField(Field):
             raise ValueError(f"Invalid value for {self.__class__.__name__}, date > 70 year")
 
 
-class GenderField(Field):
+class GenderField(CharField):
     def valid_field(self, value):
         if value not in (0, 1, 2):
             raise ValueError(f"Invalid value for {self.__class__.__name__} can only be 0 or 1 or 2")
@@ -137,7 +129,7 @@ class GenderField(Field):
             return True
 
 
-class ClientIDsField(Field):
+class ClientIDsField(CharField):
     def valid_field(self, value):
         try:
             ids_tuple = set(value)
@@ -153,15 +145,12 @@ class MethodRequest(object):
     arguments = ArgumentsField(required=True, nullable=True)
     method = CharField(required=True)
 
-    def __init__(self, account, login, token, arguments, method):
-        self.account = account
-        self.login = login
-        self.token = token
-        self.arguments = arguments
-        self.method = method
-
-    # def get_response(self):
-    #     return "None", 777
+    def __init__(self, **kwargs):
+        self.account = kwargs.get('account')
+        self.login = kwargs.get('login')
+        self.token = kwargs.get('token')
+        self.arguments = kwargs.get('arguments')
+        self.method = kwargs.get('method')
 
     @property
     def is_admin(self):
@@ -179,7 +168,10 @@ class ClientsInterestsRequest(MethodRequest):
         self.date = self.arguments.get("date")
 
     def get_response(self):
-        return "None", 777
+        res = {}
+        for cid in self.arguments.get("client_ids"):
+            res[cid] = get_interests(store=None, cid=cid)
+        return res, HTTPStatus.OK
 
 
 class OnlineScoreRequest(MethodRequest):
@@ -192,7 +184,6 @@ class OnlineScoreRequest(MethodRequest):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
         self.first_name = self.arguments.get("first_name")
         self.last_name = self.arguments.get("last_name")
         self.email = self.arguments.get("email")
@@ -220,14 +211,16 @@ class OnlineScoreRequest(MethodRequest):
             res = 42
         else:
             res = get_score(store=None, **self.arguments)
-        return {"response": res}, 200
+        return res, HTTPStatus.OK
 
 
 def check_auth(request):
     if request.is_admin:
-        digest = hashlib.sha512(datetime.datetime.now().strftime("%Y%m%d%H") + ADMIN_SALT).hexdigest()
+        to_hash = datetime.datetime.now().strftime("%Y%m%d%H") + ADMIN_SALT
     else:
-        digest = hashlib.sha512(request.account + request.login + SALT).hexdigest()
+        to_hash = request.account + request.login + SALT
+
+    digest = hashlib.sha512(to_hash.encode()).hexdigest()
     if digest == request.token:
         return True
     return False
@@ -241,15 +234,20 @@ def method_handler(request, ctx, store):
     }
 
     if request_method in supported_requests:
-        # print(request)
-        methode_request = supported_requests[request_method](**request["body"])
+        try:
+            methode_request = supported_requests[request_method](**request["body"])
+        except (ValueError, TypeError) as error:
+            return str(error), HTTPStatus.UNPROCESSABLE_ENTITY
+
     else:
         return f"Unsupported method: {request_method}", HTTPStatus.METHOD_NOT_ALLOWED
-    # ToDo Доделать аутентификацию
-    # auth = check_auth(methode_request)
-    # if not auth:
-    #     return {"code": HTTPStatus.FORBIDDEN, "error": "Forbidden"}
-    response, code = methode_request.get_response()
+
+    auth = check_auth(methode_request)
+    if not auth:
+        response = "Forbidden"
+        code = HTTPStatus.FORBIDDEN
+    else:
+        response, code = methode_request.get_response()
     return response, code
 
 
@@ -264,14 +262,14 @@ class MainHTTPHandler(server.BaseHTTPRequestHandler):
         return headers.get('HTTP_X_REQUEST_ID', uuid.uuid4().hex)
 
     def do_POST(self):
-        response, code = {}, OK
+        response, code = {}, HTTPStatus.OK
         context = {"request_id": self.get_request_id(self.headers)}
         request = None
         try:
             data_string = self.rfile.read(int(self.headers['Content-Length']))
             request = json.loads(data_string)
         except:
-            code = BAD_REQUEST
+            code = HTTPStatus.BAD_REQUEST
 
         if request:
             path = self.path.strip("/")
@@ -281,18 +279,18 @@ class MainHTTPHandler(server.BaseHTTPRequestHandler):
                     response, code = self.router[path]({"body": request, "headers": self.headers}, context, self.store)
                 except Exception as e:
                     logging.exception("Unexpected error: %s" % e)
-                    code = INTERNAL_ERROR
+                    code = HTTPStatus.INTERNAL_SERVER_ERROR
             else:
-                code = NOT_FOUND
+                code = HTTPStatus.NOT_FOUND
 
         self.send_response(code)
         self.send_header("Content-Type", "application/json")
         self.end_headers()
 
-        if code not in ERRORS:
+        if code in [HTTPStatus.OK]:
             r = {"response": response, "code": code}
         else:
-            r = {"error": response or ERRORS.get(code, "Unknown Error"), "code": code}
+            r = {"error": response, "code": code}
         context.update(r)
         logging.info(context)
         self.wfile.write(json.dumps(r).encode())
@@ -305,7 +303,7 @@ if __name__ == "__main__":
     op.add_option("-l", "--log", action="store", default=None)
     (opts, args) = op.parse_args()
     logging.basicConfig(filename=opts.log, level=logging.INFO,
-                        format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
+                        format='[%(asctime)s] %(levelname)s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
     server = server.HTTPServer(("localhost", opts.port), MainHTTPHandler)
     logging.info("Starting server at %s" % opts.port)
     try:
